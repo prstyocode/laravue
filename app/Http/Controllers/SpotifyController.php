1c2c6cf5dbd2277910de3b6fb2de0dbbd6629fdb
prstyocode <ekoprstyo.mail@gmail.com>
2020-08-26 00:16:10 +0700

    connect to spotify API

---
+++ app/Http/Controllers/SpotifyController.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\Donated;
+use App\User;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Redirect;
+
+class SpotifyController extends Controller
+{
+	private $accesToken;
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		// $this->middleware('auth');
+	}
+
+	/**
+	 * Attempt to connect with Spotify API.
+	 *
+	 * @return \Illuminate\Contracts\Support\Renderable
+	 */
+	public function connect()
+	{
+		$params = [
+			'client_id' => env('SPOTIFY_CLIENT_ID'),
+			'response_type' => 'code',
+			'redirect_uri' => env('APP_URL') . '/spotify/callback',
+			'state' => 'asdf1234',
+			'scope' => 'user-library-read user-follow-modify playlist-modify-public playlist-modify-private'
+		];
+		$query = http_build_query($params);
+		return Redirect::to('https://accounts.spotify.com/authorize?' . $query);
+	}
+
+	public function callback(Request $request)
+	{
+
+		$client = new Client();
+
+		$body = [
+			'grant_type' => 'authorization_code',
+			'code' => $request->code,
+			'redirect_uri' => env('APP_URL') . '/spotify/callback'
+		];
+
+		$response = $client->request(
+			'POST',
+			'https://accounts.spotify.com/api/token',
+			[
+				'headers' => [
+					'Authorization' => 'Basic ' . base64_encode(env('SPOTIFY_CLIENT_ID') . ':' . env('SPOTIFY_CLIENT_SECRET'))
+				],
+				'form_params' => $body
+			]
+		);
+
+		$result = json_decode($response->getBody()->getContents());
+
+		Cookie::queue('access_token', $result->access_token, $result->expires_in);
+		Cookie::queue('refresh_token', $result->refresh_token);
+
+		return redirect()->back();
+	}
+
+	public function refreshToken()
+	{
+		$client = new Client();
+
+		$refreshToken = Cookie::get('refresh_token');
+
+		if (!$refreshToken) redirect()->to('/spotify/connect')->send();
+
+		$body = [
+			'grant_type' => 'refresh_token',
+			'refresh_token' => $refreshToken
+		];
+
+		$response = $client->request(
+			'POST',
+			'https://accounts.spotify.com/api/token',
+			[
+				'headers' => [
+					'Authorization' => 'Basic ' . base64_encode(env('SPOTIFY_CLIENT_ID') . ':' . env('SPOTIFY_CLIENT_SECRET'))
+				],
+				'form_params' => $body
+			]
+		);
+
+		$result = json_decode($response->getBody()->getContents());
+
+		Cookie::queue('access_token', $result->access_token, $result->expires_in);
+
+		return $result->access_token;
+	}
+
+	private function isAuthenticated()
+	{
+		$this->accesToken = Cookie::get('access_token');
+
+		if (!$this->accesToken) {
+			$this->accesToken = $this->refreshToken();
+		}
+	}
+
+	public function getUserSavedTracks()
+	{
+
+		$this->isAuthenticated();
+
+		$client = new Client();
+
+		$response = $client->request(
+			'GET',
+			'https://api.spotify.com/v1/me/tracks',
+			[
+				'headers' => [
+					'Authorization' => 'Bearer ' . $this->accesToken,
+				],
+			]
+		);
+
+		$result = $response->getBody()->getContents();
+
+		return response()->json(json_decode($result));
+	}
+
+	public function getUsersProfile()
+	{
+
+		$this->isAuthenticated();
+
+		$client = new Client();
+
+		$response = $client->request(
+			'GET',
+			'https://api.spotify.com/v1/me',
+			[
+				'headers' => [
+					'Authorization' => 'Bearer ' . $this->accesToken,
+				],
+			]
+		);
+
+		$result = $response->getBody()->getContents();
+
+		return response()->json(json_decode($result));
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::redirect('/home', '/');
 Route::get('/listings', 'ListingController@index')->name('listing');
 Route::get('/donate', 'DonateController@index')->name('donate');
+Route::get('/spotify/connect', 'SpotifyController@connect')->name('spotify.connect');
+Route::get('/spotify/callback', 'SpotifyController@callback')->name('spotify.callback');
+Route::get('/spotify/saved-tracks', 'SpotifyController@getUserSavedTracks')->name('spotify.get-user-saved-tracks');
+Route::get('/spotify/users-profile', 'SpotifyController@getUsersProfile')->name('spotify.get-user-users-profile');
